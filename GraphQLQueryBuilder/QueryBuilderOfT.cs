838a751433df4de08fb29943f645a11e85d1bbe1
Billy Wolfington <willwolfram18@gmail.com>
2019-11-25 23:28:22 -0800

    Basic query building

--- GraphQLQueryBuilder/QueryBuilderOfT.cs
+++ GraphQLQueryBuilder/QueryBuilderOfT.cs
@@ -1,22 +1,53 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 
 namespace GraphQLQueryBuilder
 {
     public class QueryBuilder<T> where T : class
     {
+        private readonly List<string> _properties = new List<string>();
+
         public QueryBuilder<T> AddQuery(string queryName) {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            return this;
         }
 
         public QueryBuilder<T> AddProperty<TProperty>(Expression<Func<T, TProperty>> expression)
         {
-            throw new NotImplementedException();
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+
+            if (expression.Body is MemberExpression member)
+            {
+                _properties.Add(member.Member.Name);
+
+                return this;
+            }
+
+            throw new InvalidOperationException("Not a member");
         }
 
         public string Build()
         {
-            return null;
+            var query = new StringBuilder()
+                .AppendLine("query {")
+                .AppendLine("    address {");
+
+            var properties = string.Join(
+                $",{Environment.NewLine}",
+                _properties.Select(p => $"        {p}")
+            );
+
+            query.AppendLine(properties)
+                .AppendLine("    }")
+                .AppendLine("}");
+
+            return query.ToString();
         }
     }
 }
