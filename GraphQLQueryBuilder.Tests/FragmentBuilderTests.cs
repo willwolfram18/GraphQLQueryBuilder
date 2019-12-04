2fa68811204d5b2a1e4a100cd8a0b0a3c90fd944
willwolfram18 <willwolfram18@gmail.com>
2019-12-04 07:20:48 -0800

    Started working on fragments

---
+++ GraphQLQueryBuilder.Tests/FragmentBuilderTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQLQueryBuilder.Tests.Models;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace GraphQLQueryBuilder.Tests
+{
+    public class FragmentBuilderTests
+    {
+        [Fact]
+        public void IfNoFieldsSpecifiedThenEmptyFragmentIsCreated()
+        {
+            var fragment = new FragmentBuilder<Address>("EmptyAddress")
+                .Build();
+
+            Snapshot.Match(fragment);
+        }
+
+        [Fact]
+        public void ThenOnlySpecifiedFieldsInFragment()
+        {
+            var fragment = new FragmentBuilder<Address>("PartialAddress")
+                .AddField(address => address.Street1)
+                .AddField(address => address.Street2)
+                .AddField(address => address.ZipCode)
+                .Build();
+
+            Snapshot.Match(fragment);
+        }
+
+        [Fact]
+        public void ThenFieldsIncludedInFragment()
+        {
+            var fragment = new FragmentBuilder<Address>("CompleteAddress")
+                .AddField(address => address.Street1)
+                .AddField(address => address.Street2)
+                .AddField(address => address.City)
+                .AddField(address => address.State)
+                .AddField(address => address.ZipCode)
+                .Build();
+
+            Snapshot.Match(fragment);
+        }
+    }
+}
---
+++ GraphQLQueryBuilder/FragmentBuilderOfT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphQLQueryBuilder
+{
+    public class FragmentBuilder<T> where T : class
+    {
+        private readonly List<string> _fields = new List<string>();
+
+        public FragmentBuilder(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public FragmentBuilder<T> AddField<TProperty>(Expression<Func<T, TProperty>> expression)
+        {
+            if (!(expression.Body is MemberExpression member))
+            {
+                throw new InvalidOperationException();
+            }
+
+            _fields.Add(member.Member.Name);
+
+            return this;
+        }
+
+        public string Build()
+        {
+            var content = new QueryContentAppender($"fragment {Name} on {typeof(T).Name}", 0);
+
+            foreach (var field in _fields)
+            {
+                content.AppendProperty(field);
+            }
+
+            return content.ToString();
+        }
+    }
+}
