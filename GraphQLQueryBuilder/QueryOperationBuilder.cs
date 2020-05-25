1815378257d0832dbadb4cf5a085fd90490a4093
Billy Wolfington <willwolfram18@gmail.com>
2020-05-24 18:12:19 -0700

    Adding some argument handling

--- GraphQLQueryBuilder/QueryOperationBuilder.cs
+++ GraphQLQueryBuilder/QueryOperationBuilder.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace GraphQLQueryBuilder
 {
     public class QueryOperationBuilder : IGraphQLQueryContentBuilder, IQueryOperation
     {
+        private static readonly Regex GraphQLNameRegex = new Regex(@"^[_A-Za-z][_0-9A-Za-z]*$");
+
         public QueryOperationBuilder()
         {
         }
 
         public QueryOperationBuilder(string operationName)
         {
-            throw new NotImplementedException();
+            ThrowIfNameIsNotValid(operationName, "The query operation name is not valid.", nameof(operationName));
+
+            OperationName = operationName;
         }
 
         public string OperationName { get; }
@@ -19,21 +24,41 @@ namespace GraphQLQueryBuilder
 
         public IGraphQLQueryContentBuilder AddField(string field)
         {
+            ThrowIfNameIsNotValid(field, "The field name is invalid.", nameof(field));
+
             throw new NotImplementedException();
         }
 
         public IGraphQLQueryContentBuilder AddField(string alias, string field)
         {
+            ThrowIfNameIsNotValid(alias, "The alias is invalid.", nameof(alias));
+            ThrowIfNameIsNotValid(field, "The field name is invalid.", nameof(field));
+
             throw new NotImplementedException();
         }
 
         public IGraphQLQueryContentBuilder AddField(string field, ISelectionSet selectionSet)
         {
+            ThrowIfNameIsNotValid(field, "The field name is invalid.", nameof(field));
+
+            if (selectionSet == null)
+            {
+                throw new ArgumentNullException(nameof(selectionSet));
+            }
+
             throw new NotImplementedException();
         }
 
         public IGraphQLQueryContentBuilder AddField(string alias, string field, ISelectionSet selectionSet)
         {
+            ThrowIfNameIsNotValid(alias, "The alias is invalid.", nameof(alias));
+            ThrowIfNameIsNotValid(field, "The field name is invalid.", nameof(field));
+
+            if (selectionSet == null)
+            {
+                throw new ArgumentNullException(nameof(selectionSet));
+            }
+
             throw new NotImplementedException();
         }
 
@@ -45,6 +70,14 @@ namespace GraphQLQueryBuilder
         public string Build()
         {
             throw new NotImplementedException();
+        }
+
+        private static void ThrowIfNameIsNotValid(string value, string message, string argumentName)
+        {
+            if (string.IsNullOrWhiteSpace(value) || !GraphQLNameRegex.IsMatch(value))
+            {
+                throw new ArgumentException(message, argumentName);
+            }
         }
     }
 }
