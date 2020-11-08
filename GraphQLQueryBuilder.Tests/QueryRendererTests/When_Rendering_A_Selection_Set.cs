3dc4aaef747019330c46ce1aa37fc480040686ec
Billy Wolfington <willwolfram18@gmail.com>
2020-11-08 11:24:07 -0800

    Renderer tests with args

--- GraphQLQueryBuilder.Tests/QueryRendererTests/When_Rendering_A_Selection_Set.cs
+++ GraphQLQueryBuilder.Tests/QueryRendererTests/When_Rendering_A_Selection_Set.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using FluentAssertions;
+using GraphQLQueryBuilder.Abstractions.Language;
+using GraphQLQueryBuilder.Tests.ArgumentBuilderTests;
 using GraphQLQueryBuilder.Tests.Models;
 using NUnit.Framework;
 using Snapshooter.NUnit;
@@ -43,6 +45,52 @@ namespace GraphQLQueryBuilder.Tests.QueryRendererTests
             IQueryRenderer renderer = new QueryRenderer();
 
             var result = renderer.Render(selectionSet);
+
+            Snapshot.Match(result);
+        }
+
+        [Test]
+        public void Then_Arguments_Are_Rendered_Properly()
+        {
+            var nameArguments = new []
+            {
+                ArgumentBuilder.Build("initials", true),
+                ArgumentBuilder.Build("length", 3),
+                ArgumentBuilder.Build("filter", (string) null)
+            };
+            var addressArguments = new[]
+            {
+                ArgumentBuilder.Build("state", "CA"),
+                ArgumentBuilder.Build("withApartments", false),
+                ArgumentBuilder.Build("distanceFromDc", 300.25)
+            };
+
+            var addressSelectionSet = SelectionSetBuilder.For<Address>()
+                .AddScalarField("postalCode", address => address.ZipCode)
+                .Build();
+
+            var contactSelectionSet = SelectionSetBuilder.For<Contact>()
+                .AddScalarField(contact => contact.FirstName, nameArguments)
+                .AddObjectField(contact => contact.Address, addressArguments, addressSelectionSet)
+                .Build();
+
+            var contactArguments = new[]
+            {
+                ArgumentBuilder.Build("isActive", true)
+            };
+            var favoriteNumberArguments = new[]
+            {
+                ArgumentBuilder.Build("isEven", false),
+                ArgumentBuilder.Build("greaterThan", 10)
+            };
+
+            var customerSelectionSet = SelectionSetBuilder.For<Customer>()
+                .AddObjectField(customer => customer.CustomerContact, contactArguments, contactSelectionSet)
+                // TODO add in once method supports args
+//                .AddScalarCollectionField(customer => customer.FavoriteNumbers, favoriteNumberArguments)
+                .Build();
+            
+            var result = new QueryRenderer().Render(customerSelectionSet);
 
             Snapshot.Match(result);
         }
