7fb7e95b776abaff4364c3f7f821527b9ae383e5
Billy Wolfington <willwolfram18@gmail.com>
2020-11-08 19:21:55 -0800

    Added a serialization test
    Realized that the string argument values needed to be serialized when they have characters like quotes and new lines

--- GraphQLQueryBuilder.Tests/QueryRendererTests/When_Rendering_A_Selection_Set.cs
+++ GraphQLQueryBuilder.Tests/QueryRendererTests/When_Rendering_A_Selection_Set.cs
@@ -102,5 +102,25 @@ namespace GraphQLQueryBuilder.Tests.QueryRendererTests
 
             Snapshot.Match(result);
         }
+
+        [Test]
+        public void Then_String_Argument_Values_Are_Serialized()
+        {
+            var argumentValue =
+                @"Lorem ipsum dolor sit amet, ""consectetur adipiscing"" elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 'voluptate' velit esse cillum dolore eu fugiat nulla pariatur.
+
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+            var argument = ArgumentBuilder.Build("description", argumentValue);
+
+            var selectionSet = SelectionSetBuilder.For<Customer>()
+                .AddScalarField(customer => customer.Id, new[] {argument})
+                .Build();
+
+            var result = new QueryRenderer().Render(selectionSet);
+            
+            Snapshot.Match(result);
+        }
     }
 }
