d2af06696eb14b7e15ad29cf929a79a89986e023
Billy Wolfington <willwolfram18@gmail.com>
2020-11-08 19:25:50 -0800

    Wrapping string values in quotes

--- GraphQLQueryBuilder/QueryRenderer.cs
+++ GraphQLQueryBuilder/QueryRenderer.cs
@@ -126,7 +126,7 @@ namespace GraphQLQueryBuilder
             return literalValue switch
             {
                 INullArgumentValue _ => "null",
-                IStringArgumentValue stringValue => stringValue.Value,
+                IStringArgumentValue stringValue => $"\"{stringValue.Value}\"",
                 IIntegerArgumentValue intValue => intValue.Value.ToString(),
                 IFloatArgumentValue floatValue => floatValue.Value.ToString(),
                 IBooleanArgumentValue boolValue => boolValue.Value.ToString().ToLower(),
